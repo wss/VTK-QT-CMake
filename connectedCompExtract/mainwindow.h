e73c17896013c778d8c4590fd2764ccc7de0dbc1
wss <1217076109@qq.com>
2023-09-25 10:08:45 +0800

    CMake builds the connectedCompExtract example

---
+++ connectedCompExtract/mainwindow.h
@@ -0,0 +1,39 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <vtkAutoInit.h>
+VTK_MODULE_INIT(vtkRenderingOpenGL2);
+VTK_MODULE_INIT(vtkRenderingFreeType);
+VTK_MODULE_INIT(vtkInteractionStyle);
+
+#include <vtkSmartPointer.h>
+#include <vtkSphereSource.h>
+#include <vtkConeSource.h>
+#include <vtkAppendPolyData.h>
+#include <vtkPolyDataConnectivityFilter.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkActor.h>
+#include <vtkProperty.h>
+#include <vtkRenderer.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <QVTKOpenGLWidget.h>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    void connectedCompExtract();
+private:
+    Ui::MainWindow *ui;
+    QVTKOpenGLWidget* m_vtkOpenGLWidget;
+};
+#endif // MAINWINDOW_H
